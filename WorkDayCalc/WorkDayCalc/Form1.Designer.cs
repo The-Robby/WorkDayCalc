7f3465ff930f3ef7f50560faebb7a6ad82d2b822
The-Robby <145490240+The-Robby@users.noreply.github.com>
2023-10-19 14:24:55 +0200

    Solution upload

---
+++ WorkDayCalc/WorkDayCalc/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WorkDayCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtStartHour = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEndHour = new System.Windows.Forms.TextBox();
+            this.checkHalfDay = new System.Windows.Forms.CheckBox();
+            this.checkPaidLunch = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDayHours = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLunchTime = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnCalculate.Location = new System.Drawing.Point(12, 38);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(146, 63);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtStartHour
+            // 
+            this.txtStartHour.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::WorkDayCalc.Properties.Settings.Default, "StartHour", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtStartHour.Location = new System.Drawing.Point(272, 38);
+            this.txtStartHour.Name = "txtStartHour";
+            this.txtStartHour.Size = new System.Drawing.Size(100, 20);
+            this.txtStartHour.TabIndex = 1;
+            this.txtStartHour.Text = global::WorkDayCalc.Properties.Settings.Default.StartHour;
+            this.txtStartHour.TextChanged += new System.EventHandler(this.txtStartHour_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Start Hour:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "End Hour:";
+            // 
+            // txtEndHour
+            // 
+            this.txtEndHour.Location = new System.Drawing.Point(272, 80);
+            this.txtEndHour.Name = "txtEndHour";
+            this.txtEndHour.Size = new System.Drawing.Size(100, 20);
+            this.txtEndHour.TabIndex = 4;
+            this.txtEndHour.TextChanged += new System.EventHandler(this.txtEndHour_TextChanged);
+            // 
+            // checkHalfDay
+            // 
+            this.checkHalfDay.AutoSize = true;
+            this.checkHalfDay.Location = new System.Drawing.Point(424, 38);
+            this.checkHalfDay.Name = "checkHalfDay";
+            this.checkHalfDay.Size = new System.Drawing.Size(67, 17);
+            this.checkHalfDay.TabIndex = 6;
+            this.checkHalfDay.Text = "Half Day";
+            this.checkHalfDay.UseVisualStyleBackColor = true;
+            // 
+            // checkPaidLunch
+            // 
+            this.checkPaidLunch.AutoSize = true;
+            this.checkPaidLunch.Location = new System.Drawing.Point(424, 63);
+            this.checkPaidLunch.Name = "checkPaidLunch";
+            this.checkPaidLunch.Size = new System.Drawing.Size(80, 17);
+            this.checkPaidLunch.TabIndex = 7;
+            this.checkPaidLunch.Text = "Paid Lunch";
+            this.checkPaidLunch.UseVisualStyleBackColor = true;
+            this.checkPaidLunch.CheckedChanged += new System.EventHandler(this.checkPaidLunch_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(525, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 26);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "How many hours \r\ndo you have to work:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(208, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(320, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "(Everything is expressed in hours and minutes. For example: 08:15)";
+            // 
+            // txtDayHours
+            // 
+            this.txtDayHours.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::WorkDayCalc.Properties.Settings.Default, "DayHours", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtDayHours.Location = new System.Drawing.Point(638, 41);
+            this.txtDayHours.Name = "txtDayHours";
+            this.txtDayHours.Size = new System.Drawing.Size(65, 20);
+            this.txtDayHours.TabIndex = 10;
+            this.txtDayHours.Text = global::WorkDayCalc.Properties.Settings.Default.DayHours;
+            this.txtDayHours.TextChanged += new System.EventHandler(this.txtDayHours_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(525, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 26);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "How long is your \r\nlunch:";
+            // 
+            // txtLunchTime
+            // 
+            this.txtLunchTime.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::WorkDayCalc.Properties.Settings.Default, "LunchTime", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtLunchTime.Location = new System.Drawing.Point(638, 72);
+            this.txtLunchTime.Name = "txtLunchTime";
+            this.txtLunchTime.Size = new System.Drawing.Size(65, 20);
+            this.txtLunchTime.TabIndex = 12;
+            this.txtLunchTime.Text = global::WorkDayCalc.Properties.Settings.Default.LunchTime;
+            this.txtLunchTime.TextChanged += new System.EventHandler(this.txtLunchTime_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(715, 130);
+            this.Controls.Add(this.txtLunchTime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDayHours);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkPaidLunch);
+            this.Controls.Add(this.checkHalfDay);
+            this.Controls.Add(this.txtEndHour);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStartHour);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "End Hour Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtStartHour;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEndHour;
+        private System.Windows.Forms.CheckBox checkHalfDay;
+        private System.Windows.Forms.CheckBox checkPaidLunch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDayHours;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtLunchTime;
+    }
+}
+
---
+++ WorkDayCalc/WorkDayCalc/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkDayCalc
+{
+    public partial class Form1 : Form
+    {
+        class Global
+        {
+            public static string s_txtStartHour, s_txtEndHour, s_txtDayHours, s_txtLunchTime /*s_Converted*/;
+            public static double d_txtStartHour, d_txtEndHour, d_txtDayHours, d_txtLunchTime /*d_Converted*/, d_ConvertedDecimals;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtEndHour.Clear();
+            if (string.IsNullOrEmpty(txtStartHour.Text))
+            {
+                txtStartHour.SelectedText = "8:00";
+            }
+
+            Global.s_txtDayHours = txtDayHours.Text.ToString();
+            Global.s_txtLunchTime = txtLunchTime.Text.ToString();
+            Global.s_txtStartHour = txtStartHour.Text.ToString();
+
+            Global.d_txtStartHour = MinutesToDecimal(Global.s_txtStartHour);
+            Global.d_txtDayHours = MinutesToDecimal(Global.s_txtDayHours);
+            Global.d_txtLunchTime = MinutesToDecimal(Global.s_txtLunchTime);
+
+            if (checkPaidLunch.Checked && checkHalfDay.Checked)
+            {
+                Global.d_txtEndHour = Global.d_txtStartHour + (Global.d_txtDayHours/2);
+            }else if (checkPaidLunch.Checked)
+            {
+                Global.d_txtEndHour = Global.d_txtStartHour + Global.d_txtDayHours;
+            }else if (checkHalfDay.Checked)
+            {
+                Global.d_txtEndHour = Global.d_txtStartHour + (Global.d_txtDayHours/2) + Global.d_txtLunchTime;
+            }else
+            {
+                Global.d_txtEndHour = Global.d_txtStartHour + Global.d_txtDayHours + Global.d_txtLunchTime;
+            }
+
+            Global.s_txtEndHour = DecimalToMinutes(Global.d_txtEndHour);
+            //Global.s_txtEndHour = Convert.ToString(Global.d_txtEndHour);
+            txtEndHour.SelectedText += Global.s_txtEndHour;
+            Properties.Settings.Default.Save();
+        }
+
+        private void txtStartHour_TextChanged(object sender, EventArgs e)
+        {
+
+
+            Global.s_txtStartHour = txtStartHour.Text.ToString();
+            //Global.d_txtStartHour = Convert.ToDouble(Global.s_txtStartHour);
+        }
+        private void txtEndHour_TextChanged(object sender, EventArgs e)
+        {
+            //if (string.IsNullOrEmpty(txtEndHour.Text))
+            //{
+            //    txtEndHour.SelectedText = "0";
+            //}
+        }
+        public String DecimalToMinutes(double d_ToConvert) 
+        {
+            TimeSpan t_ToConvert = TimeSpan.FromHours(d_ToConvert);
+            //Global.s_Converted = Convert.ToString(t_ToConvert);
+
+            //return Global.s_Converted;
+            return t_ToConvert.Hours.ToString("00") + ":" + t_ToConvert.Minutes.ToString("00");
+        }
+        public double MinutesToDecimal(string s_ToConvert)
+        {
+            Global.d_ConvertedDecimals = Convert.ToDouble(TimeSpan.Parse(s_ToConvert).TotalHours);
+
+            return Global.d_ConvertedDecimals;
+        }
+        private void txtDayHours_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.DayHours = txtDayHours.Text.ToString();
+            
+            if (string.IsNullOrEmpty(txtDayHours.Text))
+            {
+                txtDayHours.SelectedText = "00";
+            }
+
+            Global.s_txtDayHours = txtDayHours.Text.ToString();
+        }
+        private void txtLunchTime_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.LunchTime = txtLunchTime.Text.ToString();
+
+            if (string.IsNullOrEmpty(txtLunchTime.Text))
+            {
+                txtLunchTime.SelectedText = "00";
+            }
+
+            Global.s_txtLunchTime = txtLunchTime.Text.ToString();
+        }
+        private void checkPaidLunch_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
